30ee3a7ec92634f7621544b56b57c6fb873610d1
eliotik <eliotik@gmail.com>
2024-01-17 19:57:44 -0500

    chore: Mama mia!
    Fixed some ts configs, added udner construction article - why? because Astro generates types on `sync` which result in `unknown` type for ContentEntity<'blog'> and also throws errors on build if you don't have content while trying getContentEntity func.
    What a hunt, yay! :D

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
---
+++ src/content/blog/come-back-later.md
@@ -0,0 +1,23 @@
+---
+author: Alexander
+pubDatetime: 2024-01-17T19:37:06-05:00
+modDatetime: 2024-01-17T19:37:06-05:00
+title: Hey there!
+slug: come-back-later
+featured: true
+draft: false
+tags:
+  - info
+description:
+  Blog is under construction
+---
+
+Haiiiii! (*≧ω≦) Owoh noes! ฅ^•ﻌ•^ฅ dis wittle bloggie ish under construction~ (灬º‿º灬)♡ pwease beaw with ush as we make it pwetty and sparkwy!
+
+Nyaa~ (｡♥‿♥｡) sowwy for da inconvenience~
+
+Pwease come back watew and visit ush again! (｡•́︿•̀｡)
+
+And while you wait~ here's a wittle ascii art of a cute wittle mouse for yew: ~~(__^·>
+
+(っ◔◡◔)っ Teehee~ Fank yew for understanding, wuv yew!
