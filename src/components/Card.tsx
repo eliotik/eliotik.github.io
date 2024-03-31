8b1f65179a22b91afee418ff561079bc0b804b8e
eliotik <eliotik@gmail.com>
2024-03-30 22:33:16 -0400

    feat: added optional related posts section
    - added a new thread post: flutter-google-maps-static-map
    - added optional relatedPosts property to Blog entry
      - it will be shown as a list at the end of the post
    - fixed multiple typos in previous blogs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,33 +1,39 @@
-import { slugifyStr } from "@utils/slugify";
-import type { CollectionEntry } from "astro:content";
+import { slugifyStr } from '@utils/slugify';
+import type { CollectionEntry } from 'astro:content';
 
 export interface Props {
-  href?: string;
-  frontmatter: CollectionEntry<"blog">["data"];
-  secHeading?: boolean;
+    href?: string;
+    frontmatter: CollectionEntry<'blog'>['data'];
+    secHeading?: boolean;
+    showDescription?: boolean;
 }
 
-export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, description } = frontmatter;
+export default function Card({
+    href,
+    frontmatter,
+    secHeading = true,
+    showDescription = true,
+}: Props) {
+    const { title, description } = frontmatter;
 
-  const headerProps = {
-    style: { viewTransitionName: slugifyStr(title) },
-    className: "text-lg font-medium decoration-dashed hover:underline",
-  };
+    const headerProps = {
+        style: { viewTransitionName: slugifyStr(title) },
+        className: 'text-lg font-medium decoration-dashed hover:underline',
+    };
 
-  return (
-    <li className="my-6">
-      <a
-        href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
-      >
-        {secHeading ? (
-          <h2 {...headerProps}>{title}</h2>
-        ) : (
-          <h3 {...headerProps}>{title}</h3>
-        )}
-      </a>
-      <p>{description}</p>
-    </li>
-  );
+    return (
+        <li className="my-6">
+            <a
+                href={href}
+                className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+            >
+                {secHeading ? (
+                    <h2 {...headerProps}>{title}</h2>
+                ) : (
+                    <h3 {...headerProps}>{title}</h3>
+                )}
+            </a>
+            {showDescription ? <p>{description}</p> : null}
+        </li>
+    );
 }
